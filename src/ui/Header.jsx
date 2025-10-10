9ad00a7e5fe4568a299ab007e253d73faf6245fb
RidwanRosling <ridwansbdt@gmail.com>
2025-10-10 09:52:35 +0700

    style: sidebar

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,28 +2,44 @@ import logo1 from "../assets/logo1.png";
 import { RiMovie2Fill } from "react-icons/ri";
 import { RxBookmark } from "react-icons/rx";
 import { CgProfile } from "react-icons/cg";
+import { useState } from "react";
+import { TfiArrowCircleRight } from "react-icons/tfi";
 
 export default function Header() {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
-    <nav className="w-48 h-dvh bg-[#FEF3E2]">
-      <div className="w-32 mx-auto">
+    <nav className={`${navOpen ? "w-44" : "w-28"} h-dvh bg-[#FEF3E2] fixed`}>
+      <div className={`${navOpen ? "w-32" : "W-20"} mx-auto`}>
         <img className="box-border object-contain" src={logo1} alt="" />
       </div>
       <ul className="w-full flex flex-col gap-40 mt-8 text-black">
         <li className="pl-6">
-          <RiMovie2Fill className="text-xl inline-block" />
-          <span className="pl-2 inline-block">Now Showing</span>
+          <RiMovie2Fill
+            className={`inline-block ${navOpen ? "text-2xl" : "text-4xl"}`}
+          />
+          {navOpen ? <span className="pl-2 inline-block">Browse</span> : ""}
         </li>
         <li className="pl-6">
-          <RxBookmark className="text-xl inline-block" />
-          <span className="pl-2 inline-block">Watch List</span>
+          <RxBookmark
+            className={`inline-block ${navOpen ? "text-2xl" : "text-4xl"}`}
+          />
+          {navOpen ? <span className="pl-2 inline-block">Watch List</span> : ""}
         </li>
         <li className="pl-6">
-          <CgProfile className="inline-block text-xl" />
-
-          <span className="pl-2 inline-block">Profile</span>
+          <CgProfile
+            className={`inline-block ${navOpen ? "text-2xl" : "text-4xl"}`}
+          />
+          {navOpen ? <span className="pl-2 inline-block">Profile</span> : ""}
         </li>
       </ul>
+
+      <div className="absolute top-1/2 -right-5 z-50 cursor-pointer">
+        <TfiArrowCircleRight
+          onClick={() => setNavOpen(!navOpen)}
+          className="text-3xl"
+        />
+      </div>
     </nav>
   );
 }
--- src/ui/Layout.jsx
+++ src/ui/Layout.jsx
@@ -3,9 +3,14 @@ import Header from "./Header";
 
 function AppLayout() {
   return (
-    <div>
-      <Header />
-      <main>
+    <div className="flex">
+      {/* wrapper sidebar */}
+      <div className="w-48 shrink-0">
+        <Header />
+      </div>
+
+      {/* konten utama */}
+      <main className="flex-1">
         <Outlet />
       </main>
     </div>
